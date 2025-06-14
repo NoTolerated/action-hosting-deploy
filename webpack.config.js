6eb40c54532f021bbb74509525e9b132231f53cf
NoTolerated <132168300+NoTolerated@users.noreply.github.com>
2025-06-14 05:30:46 +0100

    fix webpack config and imports

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
+  target: 'node',
+  mode: 'development',
   entry: './src/index.ts',
   output: {
     filename: 'bundle.js',
@@ -34,5 +36,10 @@ module.exports = {
       firebaseConfig: path.resolve(__dirname, 'firebaseConfig.js'),
     },
     extensions: ['.ts', '.js'],
+    fallback: {
+      path: require.resolve('path-browserify'),
+      os: require.resolve('os-browserify/browser'),
+      crypto: require.resolve('crypto-browserify'),
+    },
   },
 };
