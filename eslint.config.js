63968ad0e2c6a3fc47a7402ed706c5fbaa7dfe86
mrsimonstride <mrsimonstride@gmail.com>
2025-06-17 00:53:34 +0000

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,24 @@
-// eslint.config.js
-import js from "@eslint/js";
-
-export default [
-  js.configs.recommended,
-
-  {
-    // No languageOptions.allowTrailingCommas here
-
-    rules: {
-      // Allow trailing commas only when multiline
-      "comma-dangle": ["error", "always-multiline"],
-    },
+module.exports = {
+  "env": {
+      "browser": true,
+      "es2021": true,
+      "node": true
   },
-];
+  "extends": [
+      "eslint:recommended",
+      "plugin:react/recommended"
+  ],
+  "parserOptions": {
+      "ecmaFeatures": {
+          "jsx": true
+      },
+      "ecmaVersion": 12,
+      "sourceType": "module"
+  },
+  "plugins": [
+      "react"
+  ],
+  "ignorePatterns": ["bin/**", "bundle.js", "index.js"],
+  "rules": {
+  }
+};
