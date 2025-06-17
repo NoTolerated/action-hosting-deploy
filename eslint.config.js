15f722d398662fe726e2447b5ecb974391c6752c
NoTolerated <132168300+NoTolerated@users.noreply.github.com>
2025-06-17 03:35:28 +0100

    fix firebase rules paths and restore eslint config

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+// eslint.config.js
+import js from "@eslint/js";
+
+/** @type {import("eslint").FlatConfigItem[]} */
+export default [
+  // Use ESLint's recommended configuration
+  js.configs.recommended,
+  // Additional flat config settings can go here
+];
+
